2c237d26c59ec62869e01bcdd9ae9111787cd3e4
Aaron Cohen <aarondcohen@gmail.com>
2012-09-02 15:21:50 -0400

    - added support for filename and line to ruby - fixed bugs in setting flags during initialization

--- grepper.rb
+++ grepper.rb
@@ -8,10 +8,16 @@ require 'Grepper'
 options = Trollop::options do
 	opt :ignore_case, "Case-insensitive matching"
 	opt :invert_match, "Inverted matching", :short => '-v'
+	opt :line, "Display line number", :short => '-n'
+	opt :filename, "Display file name"
 end
 
-grepper = Grepper::Grepper.new;
-grepper.is_case_sensitive = ! options[:ignore_case]
-grepper.is_inverted = options[:invert_match]
-grepper.pattern = ARGV.shift;
+grepper = Grepper::Grepper.new(
+	:pattern => ARGV.shift,
+	:is_case_sensitive => ! options[:ignore_case],
+	:is_inverted => options[:invert_match],
+	:show_file_name => options[:filename],
+	:show_line_number => options[:line],
+);
+
 grepper.match(ARGF)
--- lib/Grepper.rb
+++ lib/Grepper.rb
@@ -4,24 +4,31 @@ class Grepper
 	attr_accessor :is_case_sensitive, :is_inverted, :pattern, :show_file_name, :show_line_number
 
 	def initialize(params = {})
-		@is_case_sensitive = params.member?(:is_case_sensitive) ? !! params.key(:is_case_sensitive) : true
-		@is_inverted = !! params.key(:is_inverted)
-		@pattern = params.key(:pattern)
-		@show_file_name = !! params.key(:show_file_name)
-		@show_line_number = !! params.key(:show_line_number)
+		@is_case_sensitive = params.member?(:is_case_sensitive) ? !! params[:is_case_sensitive] : true
+		@is_inverted = !! params[:is_inverted]
+		@pattern = params[:pattern]
+		@show_file_name = !! params[:show_file_name]
+		@show_line_number = !! params[:show_line_number]
 	end
 
 	def match(stream)
 		compiled_pattern = Regexp.new(self.pattern, self.is_case_sensitive ? nil : Regexp::IGNORECASE)
 		output_format = [
-			self.show_file_name ? "[#{stream.filename}]" : nil,
+			#self.show_file_name ? "[#{stream.filename}]" : nil,
+			self.show_file_name ? "[%{file_name}]" : nil,
 			self.show_line_number ? "[%{line_number}]" : nil,
 			'%{line}',
 		].compact.join(':');
+		last_filename = "";
 
 		stream.each_line { |line|
+			if self.show_line_number && stream.filename != last_filename
+				last_filename = stream.filename
+				stream.lineno = 1
+			end
+
 			if self.is_inverted ^ (line =~ compiled_pattern)
-				puts sprintf output_format, {:line_number => stream.lineno, :line => line}
+				puts sprintf output_format, {:file_name => stream.filename, :line_number => stream.lineno, :line => line}
 			end
 		}
 	end
